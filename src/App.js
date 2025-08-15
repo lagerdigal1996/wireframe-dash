310b2890997534983dd4bedc39127aeaeab37b22
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-08-14 23:04:05 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Wireframe Dash</h1>
+      <p>Start building your wireframe!</p>
+      {/* Here we would implement the drag-and-drop area and other components */}
+    </div>
+  );
+};
+
+export default App;
